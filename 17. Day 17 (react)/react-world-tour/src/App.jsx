09302f398102950d22e950033e0f3b62b5a40931
Paban09 <masudranapaban22@gmail.com>
2024-03-12 10:07:12 +0600

    react-state-toggle

---
+++ 17. Day 17 (react)/react-world-tour/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+
+  return (
+    <div>
+      
+      <h2>React World Tour</h2>
+      <Countries></Countries>
+    </div>
+  )
+}
+
+export default App
